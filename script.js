b5ba578c9219928361b50e55e9cd78f35726ee10
Ayaaaaaaa <106351877+Ayakamaleldeeb@users.noreply.github.com>
2024-07-29 03:00:43 +0300

    day 4

---
+++ script.js
@@ -0,0 +1,54 @@
+// Write a script that takes from the user n values and returns their sum, 
+// stop receiving values from user when he enters 0 or sum exceeds 100, 
+// check that the entered data is numeric and inform the user with the total sum of the entered values.
+//  var sum = 0;
+//  var value;
+
+//   do {
+//     value = parseFloat(prompt("Enter a number (enter 0 to stop):"));
+//     if (!isNaN(value) && value !== 0) {
+//       sum += value;
+//       console.log("Current sum:" +sum);
+//     }
+//   } while (value !== 0 && sum <= 100);
+
+//   alert('The total sum of the entered values is:' + sum);
+
+
+
+
+
+// 2. On your page, show alert for the user that say “Welcome to my site”.
+//  then show him prompt ask him to enter his name The user has to choose either (red, green or blue color).
+//  take his choice via prompt and write to the page “welcome + his name".
+
+
+// alert("Welcome to my site");
+// const userName = prompt("Please enter your name:");
+// var userColor;
+// do {
+//   userColor = prompt("Please choose a color (red, green, or blue):").toLowerCase();
+// } while (userColor !== "red" && userColor !== "green" && userColor !== "blue");
+// var textColor;
+// switch (userColor){
+//   case "red":
+//     textColor = "red";
+//     break;
+//   case "green":
+//     textColor = "green";
+//     break;
+//   case "blue":
+//     textColor = "blue";
+//     break;
+// }
+// document.write(`<h1 style="color: ${textColor};">Welcome, ${userName}!</h1>`);
+
+
+
+
+// Ask the user to enter a message then display it using the different html heading tags (from <h1> to <h6>) using Loops.
+// DO NOT write the header element explicitly in your script!
+    // const userMessage = prompt("Please enter a message:");
+    // for (var i = 1; i <= 6; i++) {
+    //   document.write(`<h${i}>${userMessage}</h${i}><br>`);
+    // }
